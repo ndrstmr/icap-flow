d43c09274a38c60b07fe8088e5e809ef468ff231
ndrs tmr <153233004+ndrstmr@users.noreply.github.com>
2025-06-17 23:07:14 +0200

    chore(CI): Integrate code style checks and security audit

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder);
